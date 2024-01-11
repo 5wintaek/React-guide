1c7d455304155b8e61e7abda9c514a1fd64dce68
Seung taek Oh <elqkddl@naver.com>
2024-01-11 16:47:04 +0900

    [Section3] Chapter54 useState 사용 및 데이터기반 가져오기 및 출력

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,13 @@ import componentImg from './assets/components.png';
 import { CORE_CONCEPTS } from './data.js';
 import { CoreConcept } from '@/components';
 import { TabButton } from '@/components';
+import { useState } from 'react';
+import { EXAMPLES } from './data.js';
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState('components');
   const onHandleClick = (selectedButton) => {
+    // selectedButton => components, jsx, props,state
+    setSelectedTopic(selectedButton);
     console.log(selectedButton);
   };
   return (
@@ -29,9 +34,16 @@ function App() {
           <menu>
             <TabButton onSelect={() => onHandleClick('components')}>Components</TabButton>
             <TabButton onSelect={() => onHandleClick('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => onHandleClick('Props')}>Props</TabButton>
-            <TabButton onSelect={() => onHandleClick('State')}>State</TabButton>
+            <TabButton onSelect={() => onHandleClick('props')}>Props</TabButton>
+            <TabButton onSelect={() => onHandleClick('state')}>State</TabButton>
           </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
